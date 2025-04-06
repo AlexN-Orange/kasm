75cd404f78d56926e016d5a909982609af121678
Nikolay Andonov <21279807+nandonov@users.noreply.github.com>
2025-04-05 21:45:26 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -6,11 +6,11 @@ const nextConfig = {
     description: 'My own workspaces.',
     icon: '/img/logo.svg',
     listUrl: 'https://github.com/nandonov/personal-kasm-registry',
-    contactUrl: 'https://kasmweb.com/support',
+    contactUrl: 'https://github.com/nandonov/personal-kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/personal-kasm-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
